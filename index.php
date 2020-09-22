ac9aa885dab196d2d279b5b3602d53517c0f9c60
Chris <FreifunkerEZ@gmail.com>
2020-09-22 22:07:54 +0200

    Adding index page for convenience.

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html>
+
+<body>
+	<a href="/kontoverwaltung.php">Kontoverwaltung</a><br><br>
+	<a href="/kontoverwaltung.php?action=findHoles">Find Holes</a><br><br>
+	<a href="/kontoverwaltung.php?action=restoreTodaysBackup">restoreTodaysBackup</a>
+</body>
+</html>
